9b2ec8413ff331c667a4f576aadb0d6a2196230e
Prashant G <prashant.gaikwad@leftrightmind.com>
2019-01-28 00:33:54 +0530

    Autocomplete UITextFields

--- AutoComplete TextField/ViewController.swift
+++ AutoComplete TextField/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import SearchTextField
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var hostNameTextField: SearchTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Set the array of strings you want to suggest
+        hostNameTextField.filterStrings(["Red", "Blue", "Yellow", "Ornage", "Black"])
+        hostNameTextField.theme.cellHeight = 50
+        hostNameTextField.maxNumberOfResults = 5
+        hostNameTextField.maxResultsListHeight = 200
+        hostNameTextField.theme.font = UIFont.systemFont(ofSize: 20)
+        hostNameTextField.setBottomBorder()
     }
 
 
 }
 
+extension UITextField {
+    
+    func setBottomBorder() {
+        self.layer.borderColor = UIColor.orange.cgColor
+        self.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+        self.layer.shadowOpacity = 1.0
+        self.layer.shadowRadius = 0.0
+    }
+    
+}
